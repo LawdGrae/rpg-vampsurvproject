be677b4f7c06fab15a45644398a264c02957a40e
honeyeezee <honeyeezee@gmail.com>
2026-09-24 23:06:47 +0800

    Added 4 character subclass of Player

---
+++ src/main/java/Character_Eumann.java
@@ -0,0 +1,16 @@
+public class Character_Eumann extends Player {
+    private static final String SPRITE_PATH = "/main/resources/character/CharEumann.png";
+    private static final double SPEED = 200.0;
+    private static final double ANIMATION_SPEED = 5.0;
+    private static final int SPRITE_SCALE = 1;
+    private static final int SPRITE_WIDTH = 64;
+    private static final int SPRITE_HEIGHT = 64;
+    private static final double MAX_HEALTH = 100.0;
+
+    public Character_Eumann () {
+        super(SPRITE_PATH, SPEED, ANIMATION_SPEED,
+            SPRITE_SCALE, SPRITE_WIDTH, SPRITE_HEIGHT, MAX_HEALTH);
+    }
+}
+    
+
---
+++ src/main/java/Character_Haze.java
@@ -0,0 +1,14 @@
+public class Character_Haze extends Player {
+    private static final String SPRITE_PATH = "/main/resources/character/CharHaze.png";
+    private static final double SPEED = 200.0;
+    private static final double ANIMATION_SPEED = 5.0;
+    private static final int SPRITE_SCALE = 1;
+    private static final int SPRITE_WIDTH = 64;
+    private static final int SPRITE_HEIGHT = 64;
+    private static final double MAX_HEALTH = 100.0;
+
+    public Character_Haze() {
+        super(SPRITE_PATH, SPEED, ANIMATION_SPEED,
+            SPRITE_SCALE, SPRITE_WIDTH, SPRITE_HEIGHT, MAX_HEALTH);
+    }
+}
